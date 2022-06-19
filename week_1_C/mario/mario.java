a5ceeb582ddc79b9176de5e60b750e45e3f6d179
‘Khaled’ <khaledbeneddine@gmail.com>
2022-06-19 12:18:23 +0200

    Update mario.java

--- week_1_C/mario/mario.java
+++ week_1_C/mario/mario.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        
         // Get the input form user
         Scanner scanner = new Scanner(System.in);
         System.out.println("Height: ");
         int height = scanner.nextInt();
+
         // Repeat until a valid input
         while (height <= 0 || height > 8) {
             System.out.println("Need a valid argument");
             System.out.println("Height: ");
             height = scanner.nextInt();
         }
+
         // Print the pyramid
         for (int i = 0; i < height; i++) {
             for (int j = height; j > 0; j--) {
